ef1d634012c942f5bd6789051101329f31bc1e19
adheebabdulla007 <adheebabdulla10@gmail.com>
2025-07-06 06:06:25 +0530

    Made JobCard clickable with Link to JobDetail

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,16 +1,19 @@
+import { Link } from "react-router-dom";
+
 function JobCard({ job }) {
   return (
-    <div className="border p-4 rounded-xl shadow-md mb-4">
-      <h2 className="text-xl font-bold">{job.title}</h2>
-      <p className="text-sm text-gray-600">
-        {job.company} • {job.location}
-      </p>
-      <p className="text-sm">
-        {job.type} | {job.experience}
-      </p>
-      <p className="font-medium text-green-700">{job.salary}</p>
-    </div>
+    <Link to={`/job/${job.id}`}>
+      <div className="border p-4 rounded-xl shadow-md mb-4 hover:bg-gray-50">
+        <h2 className="text-xl font-bold">{job.title}</h2>
+        <p className="text-sm text-gray-600">
+          {job.company} • {job.location}
+        </p>
+        <p className="text-sm">
+          {job.type} | {job.experience}
+        </p>
+        <p className="font-medium text-green-700">{job.salary}</p>
+      </div>
+    </Link>
   );
 }
-
 export default JobCard;
